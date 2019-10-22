a11bea7620e535d00aa5c1b0e4a77e46a781bc63
Owaru Ryudo <ryudoawaru@gmail.com>
2019-10-22 15:57:33 +0800

    Let view can use original helper method names

--- app/controllers/concerns/bulma_form_builder.rb
+++ app/controllers/concerns/bulma_form_builder.rb
@@ -1,4 +1,10 @@
 class BulmaFormBuilder < ::ActionView::Helpers::FormBuilder
+
+  def self.bulma_alias(method_name)
+    alias_method :"#{method_name}_without_bulma", method_name
+    alias_method method_name, :"bulma_#{method_name}"
+  end
+
   def container_tag(base_dom_class, content_or_options = nil, options = {}, &block)
     options = content_or_options if content_or_options.is_a?(Hash)
     content_or_options = @template.capture(&block) if block_given?
@@ -26,10 +32,11 @@ class BulmaFormBuilder < ::ActionView::Helpers::FormBuilder
     define_method "bulma_#{field_method_name}" do |method, options = {}|
       field_container do
         around_input(method, options) do
-          send(field_method_name, method, options)
+          send("#{field_method_name}_without_bulma", method, options)
         end
       end
     end
+    bulma_alias field_method_name
   end
 
   %i[color_field date_field email_field password_field text_field datetime_field datetime_local_field number_field phone_field range_field].each do |field_name|
@@ -39,7 +46,7 @@ class BulmaFormBuilder < ::ActionView::Helpers::FormBuilder
   def bulma_select(method, choices = nil, options = {}, html_options = {}, &block)
     field_container do
       around_input(method, options) do
-        @template.content_tag(:div, select(method, choices, options, html_options, &block), class: "select")
+        @template.content_tag(:div, select_without_bulma(method, choices, options, html_options, &block), class: "select")
       end
     end
   end
@@ -47,7 +54,7 @@ class BulmaFormBuilder < ::ActionView::Helpers::FormBuilder
   def bulma_collection_radio_buttons(method, collection, value_method, text_method, options = {}, html_options = {})
     field_container do
       label_tag(method, options) + container_tag("control") do
-        collection_radio_buttons(method, collection, value_method, text_method, options, html_options) do |b|
+        collection_radio_buttons_without_bulma(method, collection, value_method, text_method, options, html_options) do |b|
           b.radio_button + b.label(class: "radio")
         end
       end
@@ -57,12 +64,14 @@ class BulmaFormBuilder < ::ActionView::Helpers::FormBuilder
   def bulma_collection_check_boxes(method, collection, value_method, text_method, options = {}, html_options = {})
     field_container do
       label_tag(method, options) + container_tag("control") do
-        collection_check_boxes(method, collection, value_method, text_method, options, html_options) do |b|
+        collection_check_boxes_without_bulma(method, collection, value_method, text_method, options, html_options) do |b|
           b.check_box + b.label(class: "radio")
         end
       end
     end
   end
+
+  %i[select collection_radio_buttons collection_check_boxes].each{|fn| bulma_alias(fn)}
 
   private
 
